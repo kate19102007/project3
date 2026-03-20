9469a90d07ad9719747297f41fa3d9cf90942dd2
miqyes <Dana.Podoliaka@lnu.edu.ua>
2026-03-20 12:31:53 +0200

    Add files of MyProject.NegativeTests

---
+++ MyProject.NegativeTests.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "functions.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace FailedTests
+{
+    TEST_CLASS(ManualErrorTests)
+    {
+    public:
+
+        // Цей тест спеціально провалюється, щоб показати роботу Assert
+        TEST_METHOD(TestWithWrongExpectation)
+        {
+            std::vector<int> v = { 1, 2, 3 }; // Сума реально 6
+            int actual = sumOfElements(v);
+
+            // Навмисно вказуємо неправильне очікуване значення
+            int wrongExpected = 999;
+
+            // Оскільки 999 не дорівнює 6, тест СТАНЕ ЧЕРВОНИМ
+            Assert::AreEqual(wrongExpected, actual, L"Помилка: Реальна сума не збігається з очікуваною (навмисно)!");
+        }
+    };
+}
